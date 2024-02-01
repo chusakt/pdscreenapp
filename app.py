c26cf013a706cb508133c602edc7dd80ae2113a8
chusakt <chusakt@users.noreply.github.com>
2024-02-01 20:22:37 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, jsonify 
 from flask import Flask, render_template, request, jsonify 
- 
+
+app = Flask(__name__)
  
 @app.route('/api/add_message/<uuid>', methods=['GET', 'POST'])
 def add_message(uuid):
     return jsonify({"uuid":uuid})
+
+@app.route('/')
+def saysomething():
+    return ("--x-xx-x--x")
  
  
-if __name__ == '__main__':
-    serve(app, host="0.0.0.0", port=8080)
