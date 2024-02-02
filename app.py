a28e24690c1df2f55ddf4cccd2b0789ffa859c42
chusakt <chusakt@gmail.com>
2024-02-02 09:49:22 +0700

    https://github.com/chusakt/pdscreenapp.git

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask, jsonify 
 from flask import Flask, render_template, request, jsonify 
-from flask import request, Response, send_file, redirect, safe_join, abort
-
+# from flask import request, Response, send_file, redirect, safe_join, abort
 
 app = Flask(__name__)
-
-
 
 @app.route('/api/add_message/<uuid>', methods=['GET', 'POST'])
 def add_message(uuid):
@@ -16,6 +13,6 @@ def saysomething():
     return ("now what -------------")
  
  
-@app.route('/walking6min')  
-def walking6min():
-    return (" in walking function ")
+# @app.route('/walking6min')  
+# def walking6min():
+#     return (" in walking function ")
