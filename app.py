35cd88deb0dbbe75b9b45c9282b5f497a1ec744a
chusakt <chusakt@gmail.com>
2024-02-03 14:47:43 +0700

    https://github.com/chusakt/pdscreenapp.git

--- app.py
+++ app.py
@@ -646,9 +646,8 @@ def predict_tremor_rest():
             rowx = [E1,E2,E3,E4,E5,E6,E7,E8,E9,E10,E11,E12,E13,E14,E15,E16,E17,E18,E19]
             row = row + rowx
 
-
-        df3 = pd.DataFrame([rowx])
-        predictions_ = loaded_model_p.predict(df3.values)
+        df3 = pd.DataFrame([row])
+        # predictions_ = loaded_model_p.predict(df3.values)
         return jsonify({"E1":E1,
                         "E2":E2,
                         "E4":E4,
