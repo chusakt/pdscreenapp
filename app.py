3b67c4ad9cb0e83b46b1cb31740c6e682e42ad30
chusakt <chusakt@gmail.com>
2024-03-28 17:51:22 +0700

    https://github.com/chusakt/pdscreenapp.git

--- app.py
+++ app.py
@@ -2190,16 +2190,17 @@ def predict_gait_stab():
             print('check rms')
             print(row[0],row[13],row[2*13])
             print(row[0+1],row[13+1],row[(2*13)+1])
-            # if float(row[0]) < 0.05 and float(row[10]) < 0.05 and float(row[20]) < 0.05:
-            #     print('case 1')
+            if float(row[0]) < 0.05 and float(row[13]) < 0.05 and float(row[2*13]) < 0.05:
+                print('case 1')
+                return jsonify({"prediction":str(0)}) 
+            # elif float(row[2])  >= 0.001 and float(row[2])  < 0.2:
+            #     print('case 2')
             #     return jsonify({"prediction":str(0)}) 
-            # # elif float(row[2])  >= 0.001 and float(row[2])  < 0.2:
-            # #     print('case 2')
-            # #     return jsonify({"prediction":str(0)}) 
                             
             toListofNumber = [float(x) for x in row]
             X = np.array([toListofNumber])
-            predictions_ = loaded_model_gs_ag.predict(X)        
+            predictions_ = loaded_model_gs_ag.predict(X)  
+            print('by model====')      
             return jsonify({"prediction":str(predictions_[0])}) 
     except:
         return jsonify({"prediction":str(2)}) 
